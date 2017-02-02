d834f7e70b73bfa3a806becbf55262779f4940d0
sl0nwork <sl0nworkmail@gmail.com>
2017-02-02 23:59:08 +0300

    Add list of heroes with bad/good hero. To do - remove good hero.

--- src/main/java/HtmlParser.java
+++ src/main/java/HtmlParser.java
@@ -1,10 +1,7 @@
 import org.jsoup.Jsoup;
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 /**
  * Created by Anderson on 02.02.2017.
@@ -13,6 +10,10 @@ public class HtmlParser {
     public static void main(String[] args) throws Exception {
         String url = "https://ru.dotabuff.com/heroes/puck";
         String html = Jsoup.connect(url).get().html();
-        System.out.println(html);
+        Document doc = Jsoup.connect(url).get();
+        Elements content = doc.getElementsByTag("section");
+        Elements links = content.select("a[class=link-type-hero]");
+        System.out.println("-----------------------");
+        System.out.println(links);
     }
 }
