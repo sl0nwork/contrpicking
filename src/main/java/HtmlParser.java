bf0a38bd7703abc62826250e73f99a0f09d2f902
sl0nwork <sl0nworkmail@gmail.com>
2017-02-03 13:49:20 +0300

    Add Html files for loading from a file. Offline version.

--- src/main/java/HtmlParser.java
+++ src/main/java/HtmlParser.java
@@ -3,14 +3,21 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.File;
+
 /**
  * Created by Anderson on 02.02.2017.
  */
 public class HtmlParser {
     public static void main(String[] args) throws Exception {
-        String url = "https://ru.dotabuff.com/heroes/puck";
-        String html = Jsoup.connect(url).get().html();
-        Document doc = Jsoup.connect(url).get();
+
+        // Load a Doc from a File
+        File input = new File("D:/Youtube/contrpicking/src/main/resources/tmp/Puck/Puck.html");
+        Document doc = Jsoup.parse(input, "UTF-8", "http://example.com/");
+      //  String url = "https://ru.dotabuff.com/heroes/puck";
+       // String html = Jsoup.connect(url).get().html();
+        //Load a Doc from a Url
+       // Document doc = Jsoup.connect(url).get();
         Elements content = doc.getElementsByTag("section");
         Elements links = content.select("a[class=link-type-hero]");
         System.out.println("-----------------------");
